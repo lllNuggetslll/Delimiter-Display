0afc8985b64b0f3b4953517a94035b7a28854181
David <davidnguyen21@gmail.com>
2016-07-29 19:40:45 -0700

    (feat) Import in App file

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import storeConfig from './store';
 import './styles/main.scss';
+import App from './components/app/app';
+import { createStore, applyMiddleware} from 'redux';
 
 const store = storeConfig();
 
-render(
+const creatStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDom.render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
-      
+
       </Route>
     </Router>
   </Provider>,
   document.getElementById('root')
 );
+
+export default creatStoreWithMiddleware;
