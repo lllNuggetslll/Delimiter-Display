b6a9e8ab4bb0048e66dd3a40716ab96a7b5f5139
David <davidnguyen21@gmail.com>
2016-07-29 19:50:48 -0700

    (feat) Add row component

---
+++ client/src/components/render_row.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const renderRow = (rowData, onSearch) => {
+  const processedRow = rowData.map((row, index) => {
+    if (index === 0) {
+      return ([
+        <td key={index - 1} className='language'>{row.language}</td>,
+        <td
+          key={index}
+          value={row.value}
+          onClick={() => onSearch(row.value, index)}
+          className={index}>
+          {row.value}
+        </td>
+      ]);
+    } else {
+      return (
+        <td
+          key={index}
+          value={row.value}
+          onClick={() => onSearch(row.value, index)}
+          className={index}>
+          {row.value}
+        </td>)
+    }
+  })
+  return processedRow;
+}
+
+export default renderRow;
