0cdd6af5f9dc08e8ba89ed0e05e782bc5329128c
David <davidnguyen21@gmail.com>
2016-07-29 19:43:33 -0700

    (feat) Plug in middlewware

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,10 +3,12 @@ import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 import rootReducer from '../reducers';
+import Async from '../middleware/async';
 
 const logger = createLogger();
 
 const createStoreWithMiddleware = applyMiddleware(
+  Async,
   promise,
   logger,
   thunk
