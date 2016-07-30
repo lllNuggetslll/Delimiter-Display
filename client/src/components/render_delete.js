b55417d8cf936046b6334b61d2d7ba1d56c23e05
David <davidnguyen21@gmail.com>
2016-07-29 19:48:06 -0700

    (feat) Add delete button component

---
+++ client/src/components/render_delete.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const renderDelete = (props, func) => {
+  const deleteButtons = props.data.map((item, index) => {
+    return (
+      <th
+      value={index}
+      onClick={() => func(index)}
+      key={index}
+      className='btn-danger'>
+      DELETE
+      </th>
+    );
+  });
+
+  return deleteButtons
+}
+
+export default renderDelete;
