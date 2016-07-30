e04de95e6e640cecaf6b62320f3c620bc33a2fd3
David <davidnguyen21@gmail.com>
2016-07-29 19:44:43 -0700

    (feat) Add api endpoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const app = require('./config')();
 const routes = require('./api');
 const path = require('path');
+const cache = require('./models/cache');
+const controller = require('./models/controller');
 
 if (process.env.NODE_ENV === 'development') {
   const webpack = require('webpack');
@@ -13,7 +15,11 @@ if (process.env.NODE_ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 }
 
+cache.getCache('./main');
 app.use('/api', routes);
+app.get('/search/:value', function(req, res) {
+  controller.getInfo(req, res);
+})
 app.get("/*", function(req, res) {
   res.sendFile(path.resolve(__dirname, '../client/index.html'));
 });
