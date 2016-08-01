075a2e3a9b8f6e119d520b8251e2e28fe0ee5151
David <davidnguyen21@gmail.com>
2016-07-31 22:23:19 -0700

    (chore) Clean code up

--- client/src/reducers/reducer_searchbar.js
+++ client/src/reducers/reducer_searchbar.js
@@ -3,7 +3,7 @@ import { FETCH_DATA, DELETE_COLUMN, VALUE_SEARCH } from '../actions/index';
 export default function(state = -1, action) {
   switch (action.type) {
     case FETCH_DATA:
-        return [ ...state, action.payload.data ];
+      return [ ...state, action.payload.data ];
 
     case DELETE_COLUMN:
       return action.payload.data;
